f76a38cf5963b6d2c9e2a64be3f9085b27946416
KimJee <jeehoon.jk75@gmail.com>
2022-04-08 22:54:33 -0700

    Authentication is operational

--- code/main.py
+++ code/main.py
@@ -15,21 +15,15 @@ from spotipy.oauth2 import SpotifyOAuth
 if __name__ == "__main__":
     print("Hello World!")
     scope = "user-library-read" # Url extender (defines what data what we get back from API) [3 recommendation for each song]
-    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.CLIENT_ID, 
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.CLIENT_ID,
+                                                   scope=scope, 
                                                    client_secret=secrets.CLIENT_SECRET,
                                                    redirect_uri=secrets.REDIRECT_URI))
-
     results = sp.current_user_saved_tracks()
     for idx, item in enumerate(results['items']):
         track = item['track']
         print(idx, track['artists'][0]['name'], " – ", track['name'])
 
 """
-    How do we authenticate ourselves
 
-    (1) Send a (POST/GET) request to get to the correct endpoint
-
-    (2) Send out package (secrets CLIENTID/Secret)
-    (3) Response is sent by SPOTIFY (failed/suceeded/error)
-    (4) Read the response and if it is authenticated then we move on
 """
