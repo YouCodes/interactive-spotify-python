e89a1cbe1cd777b504253fcb45a254edd63a985d
KimJee <jeehoon.jk75@gmail.com>
2022-04-08 21:01:54 -0700

    Start authentication process

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # interactive-spotify-python
 A web app that lets users interact with Spotify
+
+
+## TODO
+
+* [ ] Grab any three songs that we can pull
+* [ ] Grabbing Recommendation from the Spotify API
--- code/main.py
+++ code/main.py
@@ -4,43 +4,31 @@
 """
 import secrets
 import requests
+
+# Easy way to work with Spotify API
+import spotipy
+
+# Class that bundles the credentials and sends it to JSON(?)
 from spotipy.oauth2 import SpotifyOAuth
-from spotipy.client import Spotify
-from spotipy.cache_handler import CacheFileHandler
 
-
-def get_spotify_client() -> Spotify:
-    """ Returns a spotipy client based on inside-function data. """
-    spotify_client_id = ""
-    spotify_client_secret = ""
-    spotify_redirect_uri = "http://example.com"
-    spotify_scope = "playlist-modify-private playlist-read-private"  # Allows us to read and modify private playlists
-    spotify_cache_handler = CacheFileHandler("./token.txt")
-
-    return Spotify(
-        auth_manager=SpotifyOAuth(
-            client_id=spotify_client_id,
-            SECRET =spotify_client_secret,
-            redirect_uri=spotify_redirect_uri,
-            scope=spotify_scope,
-            cache_handler=spotify_cache_handler
-        )
-    )
 
 if __name__ == "__main__":
     print("Hello World!")
+    scope = "user-library-read" # Url extender (defines what data what we get back from API) [3 recommendation for each song]
+    sp = spotipy.Spotify(auth_manager=SpotifyOAuth(client_id=secrets.CLIENT_ID, 
+                                                   client_secret=secrets.CLIENT_SECRET,
+                                                   redirect_uri=secrets.REDIRECT_URI))
 
-    x = requests.get('https://w3schools.com/python/demopage.htm')
-    print(x.text)
-
-
-    # print(secrets.CLIENT_ID)
-
+    results = sp.current_user_saved_tracks()
+    for idx, item in enumerate(results['items']):
+        track = item['track']
+        print(idx, track['artists'][0]['name'], " – ", track['name'])
 
 """
     How do we authenticate ourselves
 
     (1) Send a (POST/GET) request to get to the correct endpoint
+
     (2) Send out package (secrets CLIENTID/Secret)
     (3) Response is sent by SPOTIFY (failed/suceeded/error)
     (4) Read the response and if it is authenticated then we move on
